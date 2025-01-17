041d9dfd4f0933b83f57a65f56f91b69190dfcf3
Nate M <codebynate@gmail.com>
2025-01-17 11:16:17 +0000

    Create TrackList component

---
+++ src/components/TrackList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Track from './Track'
+
+export default function TrackList({ tracks }) {
+    const tracksList = tracks.map((track, i) => {
+        return (
+            <Track
+                key={`track-${i}`}
+                title={track.title}
+                artist={track.artist}
+            />
+        )
+    })
+
+    return (
+        <ul>
+            {tracksList}
+        </ul>
+    )
+}
