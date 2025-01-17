4c39634232daf17249112f92b3dbaac2ce8709fa
Nate M <codebynate@gmail.com>
2025-01-17 11:17:16 +0000

    Add main components to parent App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
+import SearchBar from './components/SearchBar'
+import SearchResults from './components/SearchResults'
+import Playlist from './components/Playlist'
 
 function App() {
-  return (
-    <>
-      <h1>Jammming</h1>
-    </>
-  )
+    return (
+        <>
+            <Header />
+
+            <main>
+                <div>
+                    <SearchBar />
+
+                    <div>
+                        <div>
+                            <SearchResults />
+                        </div>
+
+                        <div>
+                            <Playlist />
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default App
