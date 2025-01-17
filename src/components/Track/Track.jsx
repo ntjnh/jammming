286153447a0dd8163d46a5d4382161ef04baea0a
Nate M <codebynate@gmail.com>
2025-01-17 14:53:23 +0000

    Style track component

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './Track.module.css'
 
-export default function Track({ title, artist }) {
+export default function Track({ title, artist, list }) {
     return (
-        <div className={styles.track}>
-            <h3>{title}</h3>
-            <h4>{artist}</h4>
-        </div>
+        <li className={styles.track}>
+            <h3 className={styles.title}>{title}</h3>
+            <h4 className={styles.artist}>{artist}</h4>
+
+            <button className={styles.toggle}>{list === 'results' ? '+' : '-'}</button>
+        </li>
     )
 }
