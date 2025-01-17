0d07a7b50b336312fb87491768fa6bd717e45a54
Nate M <codebynate@gmail.com>
2025-01-17 12:34:01 +0000

    Add container to App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             <Header />
 
             <main>
-                <div>
+                <div className="container">
                     <SearchBar />
 
                     <div>
