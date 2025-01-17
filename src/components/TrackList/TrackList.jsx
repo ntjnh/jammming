e7fa02470672319d823dc635da6a22f17b0bd52b
Nate M <codebynate@gmail.com>
2025-01-17 14:53:29 +0000

    Style Tracklist component

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Track from '../Track/Track'
 import styles from './TrackList.module.css'
 
-export default function TrackList({ tracks }) {
+export default function TrackList({ tracks, list }) {
     const tracksList = tracks.map((track, i) => {
         return (
             <Track
                 key={`track-${i}`}
                 title={track.title}
                 artist={track.artist}
+                list={list}
             />
         )
     })
