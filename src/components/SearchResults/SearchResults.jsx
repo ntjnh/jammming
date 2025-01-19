9b5e3377660b6cae5c0dcc7a082332bbadea81cd
Nate M <codebynate@gmail.com>
2025-01-19 21:56:24 +0000

    Remove filtered array from SearchResults

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -3,13 +3,11 @@ import TrackList from '../TrackList/TrackList'
 import styles from './SearchResults.module.css'
 
 export default function SearchResults({ data }) {
-    const results = data.filter(track => !track.saved)
-
     return (
         <section className={styles.results}>
             <h2>Results</h2>
 
-            <TrackList list="results" tracks={results} />
+            <TrackList list="results" tracks={data} />
         </section>
     )
 }
