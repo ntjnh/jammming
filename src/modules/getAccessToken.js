ca0edcdfb102017629953dd938fcaeeb70f3a30e
Nate J.M <codebynate@gmail.com>
2025-01-26 23:16:25 +0000

    Build getAccessToken function

--- src/modules/getAccessToken.js
+++ src/modules/getAccessToken.js
@@ -1,25 +1,13 @@
-import generateRandomString from "./generateRandomString"
+const getAccessToken = (hash, setAccessToken) => {
 
-const getAccessToken = () => {
-    const client_id = import.meta.env.CLIENT_ID
-    const redirect_uri = 'http://localhost:5173/'
-    const state = generateRandomString(16)
-    const scope = `playlist-modify-private playlist-modify-public`
+    const urlHash = hash.replace('#', '')
+    const urlHashArray = urlHash.split('&')
 
-    let url = `https://accounts.spotify.com/authorize`
-    url += `?client_id=${client_id}`
-    url += `&response_type=token`
-    url += `&redirect_uri=${redirect_uri}`
-    url += `&state=you1aint3that5dude`
-    url += `&scope=${scope}`
+    const [accessToken, tokenType, expiresIn, state] = urlHashArray.map(parameter => {
+        parameter.slice(parameter.indexOf('=') + 1, parameter.length)
+    })
 
-    // https://accounts.spotify.com/authorize?client_id=ABCDEFG&response_type=token&redirect_uri=http://localhost:5173&state=you1aint3that5dude&scope=playlist-modify-private%20playlist-modify-public
-
-
-    const accessToken = ''
-    const expirationTime = ''
-
-    return accessToken
+    setAccessToken(accessToken)
 }
 
 export default getAccessToken
