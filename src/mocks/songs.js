abe9bf39f7575c3189977f5f6553c2145f8d4d86
Nate J.M <codebynate@gmail.com>
2025-09-26 02:40:31 +0100

    Create separate file for mock data

---
+++ src/mocks/songs.js
@@ -0,0 +1,156 @@
+export default {
+    tracks: {
+        items: [
+            {
+                album: {
+                    name: 'Original Sounds'
+                },
+                artists: [
+                    {
+                        name: 'Bru-C'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/0yi1bc2os`
+                },
+                id: '0yi1bc2os',
+                name: 'You & I'
+            },
+            {
+                album: {
+                    name: 'Year of the Gentleman'
+                },
+                artists: [
+                    {
+                        name: 'Ne-Yo'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/3mi4ny5yg`
+                },
+                id: '3mi4ny5yg',
+                name: 'Miss Independent'
+            },
+            {
+                album: {
+                    name: 'Sincere'
+                },
+                artists: [
+                    {
+                        name: 'Khalid'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/6au7kl8sc`
+                },
+                id: '6au7kl8sc',
+                name: 'Adore U'
+            },
+            {
+                album: {
+                    name: 'California'
+                },
+                artists: [
+                    {
+                        name: 'blink-182'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/9cf0bl1cf`
+                },
+                id: '9cf0bl1cf',
+                name: 'California'
+            },
+            {
+                album: {
+                    name: 'Chuck'
+                },
+                artists: [
+                    {
+                        name: 'Sum 41'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/2ns3sf4ck`
+                },
+                id: '2ns3sf4ck',
+                name: 'No Solution'
+            },
+            {
+                album: {
+                    name: 'Radiosurgery'
+                },
+                artists: [
+                    {
+                        name: 'New Found Glory'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/5tw6nf7rs`
+                },
+                id: '5tw6nf7rs',
+                name: 'Trainwreck'
+            },
+            {
+                album: {
+                    name: 'Dire Straits'
+                },
+                artists: [
+                    {
+                        name: 'Dire Straits'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/8ss9ds0ds`
+                },
+                id: '8ss9ds0ds',
+                name: 'Sultans of Swing'
+            },
+            {
+                album: {
+                    name: 'Family Only'
+                },
+                artists: [
+                    {
+                        name: 'Bru-C'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/1ne2bc3fo`
+                },
+                id: '1ne2bc3fo',
+                name: 'No Excuses'
+            },
+            {
+                album: {
+                    name: 'Another Love Song'
+                },
+                artists: [
+                    {
+                        name: 'Ne-Yo'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/4al5ny6al`
+                },
+                id: '4al5ny6al',
+                name: 'Another Love Song'
+            },
+            {
+                album: {
+                    name: 'Sincere'
+                },
+                artists: [
+                    {
+                        name: 'Khalid'
+                    }
+                ],
+                external_urls: {
+                    spotify: `https://open.spotify.com/track/7ew8kl9sc`
+                },
+                id: '7ew8kl9sc',
+                name: 'Everything We See'
+            }
+        ]
+    }
+}
