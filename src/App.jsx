4a57479327408b961d73570a07b51e1ecdeb99bc
Nate J.M <codebynate@gmail.com>
2025-09-26 02:48:17 +0100

    Tidy up app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,91 +3,18 @@ import './App.css'
 
 import authorisation from './modules/authorisation'
 import getToken from './modules/getToken'
+import mockResults from './mocks/songs'
 
 import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar'
 import SearchResults from './components/SearchResults/SearchResults'
 import Playlist from './components/Playlist/Playlist'
 
-const mockResults = [
-    {
-        id: '0yi1bc2os',
-        song: 'You & I',
-        artist: 'Bru-C',
-        album: 'Original Sounds',
-        uri: 'https://spotify.com/track/arandomidorsomething/you-i'
-    },
-    {
-        id: '3mi4ny5yg',
-        song: 'Miss Independent',
-        artist: 'Ne-Yo',
-        album: 'Year of the Gentleman',
-        uri: 'https://spotify.com/track/arandomidorsomething/miss-independent'
-    },
-    {
-        id: '6au7kl8sc',
-        song: 'Adore U',
-        artist: 'Khalid',
-        album: 'Sincere',
-        uri: 'https://spotify.com/track/arandomidorsomething/adore-u'
-    },
-    {
-        id: '9cf0bl1cf',
-        song: 'California',
-        artist: 'blink-182',
-        album: 'California',
-        uri: 'https://spotify.com/track/arandomidorsomething/california'
-    },
-    {
-        id: '2ns3sf4ck',
-        song: 'No Solution',
-        artist: 'Sum 41',
-        album: 'Chuck',
-        uri: 'https://spotify.com/track/arandomidorsomething/no-solution'
-    },
-    {
-        id: '5tw6nf7rs',
-        song: 'Trainwreck',
-        artist: 'New Found Glory',
-        album: 'Radiosurgery',
-        uri: 'https://spotify.com/track/arandomidorsomething/trainwreck'
-    },
-    {
-        id: '8ss9ds0ds',
-        song: 'Sultans of Swing',
-        artist: 'Dire Straits',
-        album: 'Dire Straits',
-        uri: 'https://spotify.com/track/arandomidorsomething/sultans-of-swing'
-    },
-    {
-        id: '1ne2bc3fo',
-        song: 'No Excuses',
-        artist: 'Bru-C',
-        album: 'Family Only',
-        uri: 'https://spotify.com/track/arandomidorsomething/no-excuses'
-    },
-    {
-        id: '4al5ny6al',
-        song: 'Another Love Song',
-        artist: 'Ne-Yo',
-        album: 'Another Love Song',
-        uri: 'https://spotify.com/track/arandomidorsomething/another-love-song'
-    },
-    {
-        id: '7ew8kl9sc',
-        song: 'Everything We See',
-        artist: 'Khalid',
-        album: 'Sincere',
-        uri: 'https://spotify.com/track/arandomidorsomething/everything-we-see'
-    }
-]
-
 function App() {
     const [results, setResults] = useState(mockResults)
     const [playlistName, setPlaylistName] = useState('New Playlist1')
     const [playlistTracks, setPlaylistTracks] = useState([])
-    const [playlistUris, setPlaylistUris] = useState([])
-    const [accessToken, setAccessToken] = useState(window.localStorage.access_token ? window.localStorage.access_token : '')
+    const [accessToken, setAccessToken] = useState('')
 
     const onPlaylistNameChange = e => setPlaylistName(e.target.value)
 
@@ -107,29 +34,33 @@ function App() {
         setPlaylistTracks(prev => prev.filter(track => track.id !== trackId))
     }
 
-    // const onLogin = () => {
-    //     authenticate()
-    // }
-
     useEffect(() => {
         if (window.location.pathname === "/callback") {
-            if (!window.localStorage.access_token) {
+            if (!window.localStorage.access_token ||
+                window.localStorage.access_token === 'undefined'
+            ) {
                 const generateToken = async () => await getToken(setAccessToken)
                 generateToken()
             }
+        } else if(window.localStorage.access_token ||
+                window.localStorage.access_token !== 'undefined') {
+            setAccessToken(window.localStorage.access_token)
         }
     }, [])
 
     // TODO: Delete this
     async function getProfile() {
-        const response = await fetch('https://api.spotify.com/v1/me', {
+        await fetch('https://api.spotify.com/v1/me', {
             headers: {
                 Authorization: 'Bearer ' + accessToken
             }
         })
-
-        const data = await response.json()
-        console.log(data)
+        .then(res => res.json())
+        .then(data => {
+            console.log('profile info:')
+            console.log(data)
+        })
+        .catch(e => console.error(e))
     }
 
     const onSave = async e => {
@@ -137,18 +68,7 @@ function App() {
 
         // If already authorised
         if (accessToken) {
-            // TODO: Delete this
             getProfile()
-
-            // const uris = []
-            // playlistTracks.forEach(track => uris.push(track.uri))
-            // setPlaylistUris(() => [uris])
-            
-            // // Reset existing playlist
-            // setPlaylistTracks(() => [])
-    
-            // playlistName and playlistUris to be sent to spotify
-
         } else {
             await authorisation()
         }   
@@ -161,11 +81,11 @@ function App() {
             <main>
                 <div className="bg"></div>
                 <div className="container">
-                    <SearchBar />
+                    <SearchBar accessToken={accessToken} setResults={setResults} />
 
                     <div className="columns">
                         <div className="column column--left">
-                            <SearchResults data={results} onAdd={onAdd} />
+                            <SearchResults data={results.tracks.items} onAdd={onAdd} />
                         </div>
 
                         <div className="column column--right">
