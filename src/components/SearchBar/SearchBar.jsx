67c3b7118406bb4e31cbf83694a835c7b5ac2fe6
Nate J.M <codebynate@gmail.com>
2025-09-26 02:42:19 +0100

    Add track search functionality

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,41 @@
+import { useRef } from 'react'
 import Button from '../Button/Button'
 import styles from './SearchBar.module.css'
 
-export default function SearchBar() {
+export default function SearchBar({ accessToken, setResults }) {
+    const searchRef = useRef(null)
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        const query = searchRef.current.value
+        const type = 'track'
+        const market = 'GB'
+        const limit = 10
+        let endpoint = 'https://api.spotify.com/v1/search'
+        endpoint += `?q=${query}&type=${type}&market=${market}&limit=${limit}`
+
+        await fetch(endpoint, {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        })
+        .then(response => response.json())
+        .then(data => setResults(data))
+        .catch(e => console.error(e))
+    }
+
     return (
         <section className={styles.search}>
-            <form>
-                <input type="text" name="song" id="song" placeholder="Enter a song title" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="song"
+                    id="song"
+                    ref={searchRef}
+                    placeholder="Enter a song title" />
 
-                <Button
-                    style="search"
-                    label="Search"
-                />
+                <Button style="search" label="Search" />
             </form>
         </section>
     )
