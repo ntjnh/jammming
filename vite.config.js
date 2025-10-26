53a54b36dda2fbf39c3327eec8a2ca723c50d20f
Nate J.M <codebynate@gmail.com>
2025-10-26 17:40:32 +0000

    Install vitest browser and playwright and update config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-// import process from 'node:process'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,6 +10,22 @@ export default defineConfig(({ mode }) => {
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV)
     },
-    plugins: [react()]
+    plugins: [react()],
+    test: {
+      browser: {
+        provider: 'playwright',
+        enabled: true,
+        headless: false,
+        instances: [
+          {
+            browser: 'chromium'
+          }
+        ],
+        viewport: {
+          width: 1440, 
+          height: 900
+        }
+      }
+    }
   }
 })
