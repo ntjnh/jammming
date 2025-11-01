119f4ed4ab4db03046566fab73a28f7f1437a50a
Nate <codebynate@gmail.com>
2025-11-01 12:05:41 +0000

    Write add and remove playlist tests

--- src/tests/playlist.test.jsx
+++ src/tests/playlist.test.jsx
@@ -4,23 +4,41 @@ import App from '../App'
 
 describe('Playlist', async () => {
     test('Add to playlist', async () => {
-        const { getByTestId, getByText } = await render(<App />)
+        const { getByTestId } = await render(<App />)
         const results = getByTestId('search-results')
         const playlist = getByTestId('playlist')
 
-        // Locate a track in the search results
-        // Click on its Add button
+        // Locate track in the search results
+        const californiaResult = results.getByTestId('results-california')
+
+        // Click on the track
+        await californiaResult.click()
+
         // Locate the track in the playlist
+        const california = playlist.getByTestId('playlist-california')
+
+        // Locate the track in the playlist
+        await expect.element(playlist).toContainElement(california)
     })
     
     test('Remove from playlist', async () => {
-        const { getByTestId, getByText } = await render(<App />)
+        const { getByTestId } = await render(<App />)
         const results = getByTestId('search-results')
         const playlist = getByTestId('playlist')
 
-        // Remove from playlist
-        // Locate a track in the playlist
+        // Locate track in the search results
+        const californiaResult = results.getByTestId('results-california')
+
+        // Click on the track
+        await californiaResult.click()
+
+        // Locate track in the playlist
+        const california = playlist.getByTestId('playlist-california')
+
         // Click on its Remove button
+        await california.click()
+
         // Make sure the track is no longer in the playlist
+        await expect.poll(() => expect.element(playlist).not.toContainElement(california))
     })
 })
