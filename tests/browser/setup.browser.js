67cd352cac2ab8f99f2e6bcbea220bc1c823b88e
Nate <codebynate@gmail.com>
2025-12-14 19:03:14 +0000

    Remove MSW browser worker

--- tests/browser/setup.browser.js
+++ tests/browser/setup.browser.js
@@ -1,10 +1,4 @@
-import { beforeAll, afterEach, afterAll } from 'vitest'
+import { afterAll } from 'vitest'
 import { cleanup } from 'vitest-browser-react'
-import { worker } from '../mocks/browser'
 
-beforeAll(() => worker.start({ quiet: true }))
-afterEach(() => worker.resetHandlers())
-afterAll(() => {
-    worker.stop()
-    cleanup()
-})
+afterAll(() => cleanup())
